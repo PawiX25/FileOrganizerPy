d3c7622955d0bb4fe5375160aa4fbb62be554c74
Paweł "PawiX25" Wyrębak <81538700+PawiX25@users.noreply.github.com>
2023-07-26 01:16:52 +0100

    Add option to copy files instead of moving

--- file_organizer.py
+++ file_organizer.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-def organize_files(source_folder, destination_folder):
+def organize_files(source_folder, destination_folder, move_files=True):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
@@ -9,17 +9,21 @@ def organize_files(source_folder, destination_folder):
         for filename in files:
             file_path = os.path.join(root, filename)
             if os.path.isfile(file_path):
-                file_extension = os.path.splitext(filename)[1][1:]  # File extension without the dot
+                file_extension = os.path.splitext(filename)[1][1:]  # Get the file extension without the dot
                 destination_path = os.path.join(destination_folder, file_extension)
 
                 if not os.path.exists(destination_path):
                     os.makedirs(destination_path)
 
                 try:
-                    shutil.move(file_path, os.path.join(destination_path, filename))
-                    print(f"Moved '{filename}' to '{destination_path}'")
+                    if move_files:
+                        shutil.move(file_path, os.path.join(destination_path, filename))
+                        print(f"Moved '{filename}' to '{destination_path}'")
+                    else:
+                        shutil.copy2(file_path, os.path.join(destination_path, filename))
+                        print(f"Copied '{filename}' to '{destination_path}'")
                 except shutil.Error:
-                    print(f"Error moving '{filename}' to '{destination_path}'")
+                    print(f"Error processing '{filename}'")
 
 if __name__ == "__main__":
     print("Example Paths:")
@@ -36,5 +40,12 @@ if __name__ == "__main__":
         source_folder = input("Enter the source folder path: ")
 
     destination_folder = input("Enter the destination folder path: ")
+    
+    copy_files = input("Do you want to copy the files instead of moving them? (yes/no): ").lower()
 
-    organize_files(source_folder, destination_folder)
+    if copy_files == "yes":
+        move_files = False
+    else:
+        move_files = True
+
+    organize_files(source_folder, destination_folder, move_files)
